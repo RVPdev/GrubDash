7d27a564d1145697ec8581876e8f5f0b20f31b42
RenatoDevi <renatovilcahuaman@gmail.com>
2023-08-14 15:11:58 -0500

    seventh

--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -9,12 +9,63 @@ const nextId = require("../utils/nextId");
 // TODO: Implement the /orders handlers needed to make the tests pass
 
 function list(req, res) {
-    res.json({data: orders});
+  res.json({ data: orders });
 }
 
+function hasDeliver(req, res, next) {
+  const { deliverTo } = req.body.data;
+  if (!deliverTo || deliverTo === "") {
+    return next({
+      status: 400,
+      message: "Order must include a deliverTo",
+    });
+  }
+  next();
+}
 
+function hasNumber(req, res, next) {
+  const { mobileNumber } = req.body.data;
+  if (!mobileNumber || mobileNumber === "") {
+    return next({
+      status: 400,
+      message: "Order must include a mobileNumber",
+    });
+  }
+  next();
+}
 
+function hasDishes(req, res, next) {
+  const { dishes } = req.body.data;
+  if (!dishes) {
+    return next({
+      status: 400,
+      message: "Order must include a dish",
+    });
+  } else if (dishes.length === 0 || !Array.isArray(dishes)) {
+    return next({
+      status: 400,
+      message: "Order must include at least one dish",
+    });
+  } else {
+    next();
+  }
+}
+
+function create(req, res) {
+  const { deliverTo, mobileNumber, status, dishes } = req.body.data;
+  const newOrder = {
+    id: nextId(),
+    deliverTo,
+    mobileNumber,
+    status,
+    dishes,
+  };
+
+  orders.push(newOrder);
+  res.status(201).json({ data: newOrder });
+}
 
 module.exports = {
-    list,
-}
+  create: [hasDeliver, hasNumber, hasDishes, create],
+  list,
+};
